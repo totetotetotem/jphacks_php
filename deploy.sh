c95ab8ef00d0b5509567ca6e30493538825f3e59
totetotetotem <kyarioka7945@gmail.com>
2016-10-18 09:18:55 +0900

    deploy bot enable to post to slack

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 git pull origin master
-systemctl restart uthackers_app
 nginx -s reload
+systemctl restart uthackers_app
+
+sleep 5s
 
 res = `curl -f http://localhost`
 if [ $res == 0] ; then
+    curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "deploy_bot", "text": "deploy success"}' https://hooks.slack.com/services/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
+else
+    curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "deploy_bot", "text": "deploy failed"}' https://hooks.slack.com/services/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
 fi
