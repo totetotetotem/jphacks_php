d72cabaabd853a92acc2301c1a27c6377d9341dc
totetotetotem <kyarioka7945@gmail.com>
2016-10-23 23:19:47 +0900

    enable line_reply

--- src/routes.php
+++ src/routes.php
@@ -1,34 +1,14 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-        // Sample log message
-        $this->logger->info("Slim-Skeleton '/' route");
-
-        // Render index view
-        return $this->renderer->render($response, 'index.phtml', $args);
-        });
-
 $app->post('/line', function($request, $response, $args) {
-        $json_string = file_get_contents('php://input');
-        $json_object = json_decode($json_string);
-
-        foreach ($json_object->events as $event) {
-        if('message' == $event->type) {
-        api_post_request($event->replyToken, $event->messge->text);
-        }else if('beacon' == $event->type) {
-        /* ここで賞味期限の近い食べ物のリストをとってきてメッセージを組み立てる */
-        api_post_request($event->replyToken, 'Beacon event');
-        }
-        }
-
         function api_post_request($token, $message) {
         $url = 'https://api.line.me/v2/bot/message/reply';
         $channel_access_token = getenv("LINE_CHANNEL_ACCESS_TOKEN");
         $headers = array(
             'Content-type: application/json',
             "Authorization: Bearer {$channel_access_token}"
-            );
+	);
         $post = array(
             'replyToken' => $token,
             'messages' => array(
@@ -44,6 +24,43 @@ $app->post('/line', function($request, $response, $args) {
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_exec($curl);
+        $result = curl_exec($curl);
+	curl_close($curl);
+	return $result;
         }
+
+        $this->logger->addDebug(getenv("LINE_CHANNEL_ACCESS_TOKEN"));
+        $json_string = file_get_contents('php://input');
+        $json_object = json_decode($json_string);
+
+	var_dump($json_object);
+	$this->logger->addDebug("json_string".$json_string);
+
+	$result = "";
+	if(isset($json_object->events)) {
+        foreach ($json_object->events as $event) {
+        	if('message' == $event->type) {
+        		$result = api_post_request($event->replyToken, $event->message->text);
+        	}else if('beacon' == $event->type) {
+        	// ここで賞味期限の近い食べ物のリストをとってきてメッセージを組み立てる 
+	        	api_post_request($event->replyToken, 'Beacon event');
+        	}
+        }
+	}
+
+	$data =[
+        	"status" => "OK",
+		"result" => $result
+    	];
+
+    	return $response->withJson($data);
+
+        });
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $this->renderer->render($response, 'index.phtml', $args);
         });
