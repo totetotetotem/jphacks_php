e152605a1d4722b2f3adb01bfa4be3cac86e1e21
totetotetotem <kyarioka7945@gmail.com>
2016-10-23 16:17:40 +0900

    add line_bot beacon_reply

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,48 @@
 // Routes
 
 $app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+        // Render index view
+        return $this->renderer->render($response, 'index.phtml', $args);
+        });
+
+$app->post('/line', function($request, $response, $args) {
+        $json_string = file_get_contents('php://input');
+        $json_object = json_decode($json_string);
+
+        foreach ($json_object->events as $event) {
+        if('message' == $event->type) {
+        api_post_request($event->replyToken, $event->messge->text);
+        }else if('beacon' == $event->type) {
+        /* ここで賞味期限の近い食べ物のリストをとってきてメッセージを組み立てる */
+        api_post_request($event->replyToken, 'Beacon event');
+        }
+        }
+
+        funciton api_post_request($token, $message) {
+        $url = 'https://api.line.me/v2/bot/message/reply';
+        $channel_access_token = getenv("LINE_CHANNEL_ACCESS_TOKEN");
+        $headers = array(
+            'Content-type: application/json',
+            "Authorization: Bearer {$channel_access_token}"
+            );
+        $post = array(
+            'replyToken' => $token,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $message
+                    )
+                )
+            );
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_exec($curl);
+        }
+        });
