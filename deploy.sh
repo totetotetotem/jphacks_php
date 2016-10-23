0a1949f9cfae68b150d632f963ca4ba9c7ffc388
totetotetotem <kyarioka7945@gmail.com>
2016-10-23 15:46:47 +0900

    deploy.sh変えた

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ systemctl restart uthackers_app
 
 sleep 5s
 
-res = `curl -f http://localhost`
-if [ $res == 0] ; then
+res=`curl -f http://localhost 2>&1`
+res=`echo "$res" | grep 22`
+if [ -z "$res" ]; then
     curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "deploy_bot", "text": "deploy success"}' https://hooks.slack.com/services/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
 else
     curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "deploy_bot", "text": "deploy failed"}' https://hooks.slack.com/services/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
