1e69c269912831675b0696016e93a265999b8f6b
Turenar <sora@turenar.xyz>
2016-10-24 14:54:22 +0900

    add propel conf include directive

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,6 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// propel
+require_once __DIR__ . '/../db/generated-conf/config.php';
