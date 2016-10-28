4969742e24a8900d2eabb2e123b314bf7216c412
totetotetotem <kyarioka7945@gmail.com>
2016-10-29 08:53:04 +0900

    add decrement

--- src/routes/item.php
+++ src/routes/item.php
@@ -3,11 +3,32 @@
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-$app->get('/item/:familyId', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    $items = ItemQuery::create()
-            ->filterByFamilyId($familyId)
-            ->orderByExpireDate()
-            ->find();
+$app->get('/item/{familyId}', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    //TODO decほどじゃないけどトークンを比較すべき
 
-	return $response->withJson($items);
+    $family = \ORM\FamilyQuery::create()->filterByFamilyId((int)$args['familyId'])->findPk(1);
+
+    $query = \ORM\ItemQuery::create()
+            ->orderByExpireDate();
+
+    $items = $family->getItems($query);
+
+    $json = $items->toJson();
+
+    return $response->getBody()->write($json);
 });
+
+$app->post('/item/{itemId}/dec', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    //TODO トークンと比較してきちんとユーザー確かめる
+    //TODO ひとつずつのデクリメントじゃなくて、クライアントでちょっとは蓄えてからポストしてまとめて減らす
+    transaction(function() {
+	/*
+	$item = \ORM\ItemQuery::create()->filterByItemId((int)$args['itemId'])->findPk(1);
+        $data = (int)$item->getCountOfItem()
+        $item->setCountOfItem((string)($data - 1))
+             ->save();
+	*/
+    });
+
+    return  get_renderer()->render($response);
+}
