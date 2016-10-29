adabafa11a7460f556a41d5d9c8c3a5fed3699bc
Turenar <sora@turenar.xyz>
2016-10-29 10:41:46 +0900

    RequestValidateMiddlwareをそれぞれのAPIにつけるように
    危険だけどまあしゃあないかなあ(

--- src/middleware/RequestValidateMiddleware.php
+++ src/middleware/RequestValidateMiddleware.php
@@ -1,7 +1,22 @@
 <?php
+namespace middleware;
+
+use JsonSchema\Validator;
+use Psr\Http\Message\ResponseInterface;
 
 class RequestValidateMiddleware
 {
+	/**
+	 * RequestValidateMiddleware constructor.
+	 * @param string|null $yaml_path チェック用のyamlファイルパス (generated-api-schemaから相対パス)
+	 * @param bool $check_out 出力をチェックするかどうか
+	 */
+	public function __construct($yaml_path = null, $check_out = true)
+	{
+		$this->yaml_path = $yaml_path;
+		$this->check_out = $check_out;
+	}
+
 	/**
 	 * Request Validation
 	 *
@@ -13,7 +28,8 @@ class RequestValidateMiddleware
 	 */
 	public function __invoke($request, $response, $next)
 	{
-		$schema_file = APP_ROOT_PATH . '/generated-api-schema/' . $request->getRequestTarget() . '.json';
+		$request_target = $this->yaml_path ?? $request->getRequestTarget();
+		$schema_file = APP_ROOT_PATH . '/generated-api-schema/' . $request_target . '.json';
 		$response = $response->withHeader('Content-Type', 'application/json;charset=utf-8');
 		if (!file_exists($schema_file)) {
 			// エラー出力
@@ -28,10 +44,21 @@ class RequestValidateMiddleware
 			return get_renderer()->renderAsError($response, 400, 'Invalid request', 'malformed json');
 		}
 
-		$validator = new JsonSchema\Validator();
+		$validator = new Validator();
 		$validator->check($data, $schema->input);
 		if ($validator->isValid()) {
-			return $next($request, $response);
+			/** @var ResponseInterface $real_response */
+			$real_response = $next($request, $response);
+			$validator->check(json_decode($real_response->getBody()->getContents()), $schema->output);
+			if ($validator->isValid()) {
+				return $real_response;
+			} else {
+				$extra = [];
+				foreach ($validator->getErrors() as $error) {
+					$extra[] = sprintf('[%s] %s', $error['property'], $error['message']);
+				}
+				return get_renderer()->renderAsError($response, 500, 'Invalid request', 'output validation failed', $extra);
+			}
 		} else {
 			$extra = [];
 			foreach ($validator->getErrors() as $error) {
--- src/routes/user.php
+++ src/routes/user.php
@@ -1,18 +1,28 @@
 <?php
 
+use middleware\RequestValidateMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-$app->post('/user/add', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-	transaction(function () {
-		$family = new \ORM\Family();
-		$family->save();
+$app->group('/user', function () {
+	$this->post('/add', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+		$data = transaction(function () {
+			$family = new \ORM\Family();
+			$family->setToken(sha1(mt_rand() . uniqid(gethostname(), true)))
+				->save();
 
-		$user = new \ORM\User();
-		$user->setAccessToken(sha1(mt_rand() . uniqid(gethostname(), true)))
-			->setFamilyId($family->getFamilyId())
-			->save();
-	});
+			$user = new \ORM\User();
+			$user->setAccessToken(sha1(mt_rand() . uniqid(gethostname(), true)))
+				->setFamilyId($family->getFamilyId())
+				->save();
 
-	return get_renderer()->render($response);
+			return [
+				'user' => [
+					'access_token' => $user->getAccessToken(),
+					'family_id' => $user->getFamilyId()],
+				'family' => [
+					'token' => $family->getToken()]];
+		});
+		return get_renderer()->render($response, $data);
+	})->add(new RequestValidateMiddleware());
 });
