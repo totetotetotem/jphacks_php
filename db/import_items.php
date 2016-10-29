d820ffdf5f8489240ea598039cdbbccf0da38785
Turenar <sora@turenar.xyz>
2016-10-30 02:50:13 +0900

    fix require failed

--- db/import_items.php
+++ db/import_items.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . 'generated-conf/config.php';
+require_once __DIR__ . '/generated-conf/config.php';
 $fp = fopen('php://stdin', 'r');
 
 while (($buf = fgets($fp)) !== false) {
